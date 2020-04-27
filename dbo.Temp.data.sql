4edad9a62ddf1a36fa35fb206c9e9e2bfb0de375
Vpopazov <64418159+Vpopazov@users.noreply.github.com>
2020-04-27 17:18:06 +0300

    Add files via upload

---
+++ dbo.Temp.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[Temp] ([ID], [Name], [Last], [Dates]) VALUES (4, N'Vanyo', N'Popazov', N'2020-04-07')
+INSERT INTO [dbo].[Temp] ([ID], [Name], [Last], [Dates], [Time]) VALUES (5, N'Presiyan', N'Tigara', N'2020-04-22', N'11:52')
